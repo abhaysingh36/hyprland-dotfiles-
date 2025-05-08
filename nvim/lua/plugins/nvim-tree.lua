60ff0a6fc598ee098c47888c8165975ee330168f
abhaysingh <abhaysingcom0@gmail.com>
2025-05-08 18:08:32 +0530

    abs tested

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -2,28 +2,34 @@ return {
 	{
 		"nvim-tree/nvim-tree.lua",
 		dependencies = {
-			"nvim-tree/nvim-web-devicons", -- Optional: for file icons
-			"nvim-lua/plenary.nvim", -- Required by Telescope
-			"nvim-telescope/telescope.nvim", -- Fuzzy finder
+			"nvim-tree/nvim-web-devicons",
+			"nvim-lua/plenary.nvim",
+			"nvim-telescope/telescope.nvim",
 		},
 		keys = {
 			{ "<leader>e", "<cmd>NvimTreeToggle<cr>", desc = "Toggle NvimTree" },
 		},
 		opts = {
-			disable_netrw = false,
+			disable_netrw = true,
 			hijack_netrw = true,
 			filters = {
 				dotfiles = true,
+				exclude = { ".cconfig" }, -- Show .cconfig files
 			},
 			view = {
 				width = 50,
 				side = "right",
 			},
 			live_filter = {
-				always_show_folders = false, -- Allow folders to be filtered
+				always_show_folders = false,
 			},
 			on_attach = function(bufnr)
 				local api = require("nvim-tree.api")
+				local lib = require("nvim-tree.lib")
+				local view = require("nvim-tree.view")
+				local core = require("nvim-tree.core")
+				local utils = require("nvim-tree.utils")
+
 				local function opts(desc)
 					return {
 						desc = "nvim-tree: " .. desc,
@@ -34,23 +40,76 @@ return {
 					}
 				end
 
-				-- Minimal mappings to avoid conflicts
+				-- Function to check if a node or its children match the query
+				local function node_or_children_match(node, query)
+					local node_name = node.name:lower()
+					if node_name:find(query:lower()) then
+						return true
+					end
+					if node.nodes then -- If it's a folder, check its children
+						for _, child in ipairs(node.nodes) do
+							if child.name:lower():find(query:lower()) then
+								return true
+							end
+						end
+					end
+					return false
+				end
+
+				-- Custom live filter implementation
+				local function custom_live_filter()
+					local query = vim.fn.input("[CUSTOM FILTER]: ")
+					if query == "" then
+						api.tree.reload()
+						return
+					end
+
+					-- Get the current tree nodes
+					local tree = core.get_explorer()
+					if not tree then
+						return
+					end
+
+					-- Traverse all nodes and filter based on query
+					local function filter_nodes(nodes)
+						local filtered = {}
+						for _, node in ipairs(nodes) do
+							if node_or_children_match(node, query) then
+								-- If the node matches or has matching children, keep it
+								table.insert(filtered, node)
+								if node.nodes then
+									node.nodes = filter_nodes(node.nodes) -- Recursively filter children
+								end
+							end
+						end
+						return filtered
+					end
+
+					-- Apply the filter
+					tree.nodes = filter_nodes(tree.nodes)
+					api.tree.reload()
+				end
+
+				-- Existing mappings
 				vim.keymap.set("n", "<C-]>", api.tree.change_root_to_node, opts("CD"))
 				vim.keymap.set("n", "<C-e>", api.node.open.edit, opts("Open"))
 				vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
 				vim.keymap.set("n", "a", api.fs.create, opts("Create"))
 				vim.keymap.set("n", "d", api.fs.remove, opts("Delete"))
 				vim.keymap.set("n", "r", api.fs.rename, opts("Rename"))
-
-				-- Custom focus mappings
 				vim.keymap.set("n", "t", api.tree.focus, opts("Focus Tree"))
 				vim.keymap.set("n", "e", "<C-w>h", opts("Focus Left Window (Editor)"))
 
-				-- Live filter mappings
-				vim.keymap.set("n", "/", api.live_filter.start, opts("Start Live Filter"))
-				vim.keymap.set("n", "?", api.live_filter.clear, opts("Clear Live Filter"))
+				-- Use custom live filter
+				vim.keymap.set("n", "/", function()
+					api.tree.collapse_all(true)
+					custom_live_filter()
+				end, opts("Start Custom Live Filter"))
 
-				-- Telescope fuzzy find
+				vim.keymap.set("n", "?", function()
+					api.tree.reload() -- Clear filter by reloading the tree
+				end, opts("Clear Live Filter"))
+
 				vim.keymap.set("n", "<leader>f", function()
 					local node = api.tree.get_node_under_cursor()
 					local path = node and node.absolute_path or vim.fn.getcwd()
@@ -64,8 +123,6 @@ return {
 		},
 		config = function(_, opts)
 			require("nvim-tree").setup(opts)
-
-			-- Autocommand to increase font size for NvimTree (for GUI clients)
 			vim.api.nvim_create_autocmd("FileType", {
 				pattern = "NvimTree",
 				callback = function()
