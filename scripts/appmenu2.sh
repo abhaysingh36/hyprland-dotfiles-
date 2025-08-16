4b9ffc8092e00b1926da78b42b9ef0cc97bc2728
abhaysingh <abhaysingcom0@gmail.com>
2025-08-16 22:27:55 +0530

    script update

--- scripts/appmenu2.sh
+++ scripts/appmenu2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true; do
-    choice=$(printf "List Pacman Packages\nList AUR Packages\nList Core Packages\nList Extra Packages\nSearch Package\nInstall Package\nRemove Package\nUpdate System\nExit" \
-        | fzf --prompt="Package Manager > " --height=12)
+    choice=$(printf "List Pacman Packages\nList AUR Packages\nList Core Packages\nList Extra Packages\nList Explicitly Installed (Native)\nList Explicitly Installed (AUR)\nList Flatpak Packages\nSearch Package\nInstall Package\nRemove Package\nRemove Flatpak Package\nUpdate System\nExit" \
+        | fzf --prompt="Package Manager > " --height=16)
 
     case "$choice" in
         "List Pacman Packages")
@@ -16,6 +16,16 @@ while true; do
         "List Extra Packages")
             pacman -Sl extra | grep '\[installed\]' | awk '{print $2}' | fzf --prompt="Extra Installed > " --preview 'pacman -Qi {}'
             ;;
+        "List Explicitly Installed (Native)")
+            pacman -Qent | fzf --prompt="Explicit Native > " --preview 'pacman -Qi {1}'
+            ;;
+        "List Explicitly Installed (AUR)")
+            pacman -Qemt | fzf --prompt="Explicit AUR > " --preview 'pacman -Qi {1}'
+            ;;
+        "List Flatpak Packages")
+            flatpak list --columns=application,version,origin | \
+                fzf --prompt="Flatpak Installed > " --preview 'flatpak info {1}'
+            ;;
         "Search Package")
             read -p "Enter search term: " term
             yay -Ss "$term" | less
@@ -27,6 +37,10 @@ while true; do
         "Remove Package")
             pkg=$(pacman -Qe | fzf --prompt="Select to remove > " | awk '{print $1}')
             [ -n "$pkg" ] && yay -Rns "$pkg"
+            ;;
+        "Remove Flatpak Package")
+            pkg=$(flatpak list --columns=application | fzf --prompt="Select Flatpak to remove > ")
+            [ -n "$pkg" ] && flatpak uninstall "$pkg"
             ;;
         "Update System")
             yay -Syu
